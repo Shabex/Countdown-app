8a0f647658774558036435bfde2320e919ef98a0
Shabex <shabsibrah@gmail.com>
2022-06-29 18:44:00 +0300

    countdown app

---
+++ index.js
@@ -0,0 +1,22 @@
+const countdown = ()=> {
+    const countDate = new Date('June 30, 2022 19:00:00').getTime()
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const seconds = 1000;
+    const minutes = seconds*60;
+    const hours = minutes* 60;
+    const days = hours* 24;
+
+    const textDay = Math.floor(gap/days);
+    const textHour = Math.floor ((gap%days)/hours);
+    const textMinute = Math.floor ((gap%hours)/minutes);
+    const textSecond = Math.floor ((gap%minutes)/seconds);
+
+    document.querySelector(".days").innerText = textDay
+    document.querySelector(".hours").innerText = textHour
+    document.querySelector(".minutes").innerText = textMinute
+    document.querySelector(".seconds").innerText = textSecond
+    
+};
+setInterval(countdown, 1000);
